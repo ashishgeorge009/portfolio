8f0759e048839958897b1ac29135e7ab899aeec0
Ashish Varghese M <ashgeorge009@gmail.com>
2020-10-02 03:59:53 +0530

    port2.0

---
+++ js/ashish.js
@@ -0,0 +1,60 @@
+const spanDetector = document.getElementById("span-detector");
+// const nav = document.getElementById("nav");
+// const showCase = document.querySelector(".showcase");
+// const featureSection = document.querySelector(".features");
+
+
+
+if(spanDetector){
+    
+window.addEventListener("load",function(){
+    const clients = ["SOFTWARE DEVELOPMENT","DATA SCIENCE","DEEP LEARNING","ENTREPRENEUR"];
+    typeWriter(spanDetector,clients);
+    
+    // window.addEventListener("scroll", myfunction);
+    
+});
+}
+
+function typeWriter(spanDetector,clients)
+{
+    let txt ="";
+    let wordIndex = 0;
+    let isDeleting = false;
+    function typer(){
+        let wait=30;
+        wordIndex = wordIndex%clients.length;
+        const word = clients[wordIndex];
+        if (isDeleting==true ){
+            txt = word.substring(0,txt.length-1)
+        }
+        else{
+            txt = word.substring(0,txt.length +1)
+        }
+        spanDetector.textContent=txt;
+        const lpause= 2000;
+        if(isDeleting==true && txt == ""){
+            wordIndex++;
+            isDeleting=false;
+        }
+        // else if(isDeleting==true && txt.length==word.length){
+        //     isDeleting=false;
+        //     wait = lpause;
+        // }
+        else if(isDeleting == false && txt.length ==word.length){
+            isDeleting=true;
+            wait = lpause;
+        }
+    
+
+    setTimeout(
+        
+        
+        function(){
+
+            // console.log("I was printed after a second");
+            typer();
+        },wait)
+    }
+    typer();
+}
